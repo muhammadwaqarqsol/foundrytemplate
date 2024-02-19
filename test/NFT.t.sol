ad3ef773ba2eabe4c9377b7a9135b5b068332924
Muhammad Waqar <muwaqar.qsol@gmail.com>
2024-02-19 19:07:35 +0500

    NFT CONTRACT ADDED

---
+++ test/NFT.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import {Test, console} from "lib/forge-std/src/Test.sol";
+import {NFT} from "../src/NFT.sol";
+
+contract NFT_Test is Test {
+  
+}
